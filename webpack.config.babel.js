80e41e58cfa4abae220c1c792a7c8901e7dc81a9
Thomas Carlson <thomas@siberia.io>
2016-11-16 18:39:38 -0500

    Remove HtmlWebpackPlugin from webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,7 +5,6 @@ import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import autoprefixer from 'autoprefixer';
 import nested from 'postcss-nested';
 import webpack from 'webpack';
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 import config from './config';
 
