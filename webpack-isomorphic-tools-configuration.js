1115c3bc92c7f0912dbdac3b3fe0d8f0ae8579b0
Thomas Carlson <thomas@siberia.io>
2016-11-16 13:40:08 -0500

    Remove debug from webpackIsomorphicTools config

--- webpack-isomorphic-tools-configuration.js
+++ webpack-isomorphic-tools-configuration.js
@@ -1,7 +1,6 @@
 import WebpackIsomorphicTools from 'webpack-isomorphic-tools/plugin';
 
 module.exports = {
-  debug: true,
   webpack_assets_file_path: './webpack-assets.json',
   assets: {
     images: {
